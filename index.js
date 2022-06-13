8322f3a712ffb457ff4967e3973610b071dd6778
Nam Nguyen <namnguyen140598@gmail.com>
2022-06-13 23:02:24 +0700

    Initialize

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+//get used for route
+app.get('/trang-chu', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
